e84becea4be3cc7e9ad2030ffc341acd24dc651d
Nicolas Sosa <netsosa@hotmail.com>
2024-11-01 09:37:13 -0300

    Primera entrega 2

--- js/main.js
+++ js/main.js
@@ -12,34 +12,38 @@ function opcionValida(opciones){
     }
     return true
 }
-
+function caso1(){
+    alert ("Haz llegado al lugar correcto")
+}
+function caso2(){
+    alert ("Te Invito a que visites nuestro otro sitio web dedicado solamente a CM")
+}
+function caso3(){
+    alert ("Espero que todo lo que leas te sea util para tu busqueda")
+}
 //Inicio del programa
 
 alert ("Bienvenido a NETSOSA")
 const opciones = "¿Qué esta buscando? 1- Crear mi pagina web 2- Un Community Manager 3- Solo informarme 0- Salir"
 
-let opcion =parseInt(prompt(opciones))
-
-const caso1 = alert ("Haz llegado al lugar correcto")
-const caso2 = alert ("Te Invito a que visites nuestro otro sitio web dedicado solamente a CM")
-const caso3 = alert ("Espero que todo lo que leas te sea util para tu busqueda")
+let opcion =parseInt(prompt(opciones)) 
 
 while (opcionValida(opcion)){
 
     switch(opcion){
 
         case 1:
-            console.log(caso1)
+            caso1
             break;
             
 
         case 2:
-            console.log(caso2)
+            caso2
             break;
 
 
         case 3:
-            console.log(caso3)
+            caso3
             break;
     }
 
